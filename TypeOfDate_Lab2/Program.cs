fdfe5017ca6797f2c11ee023cbc4d2906d8d9213
Денис Лихачев <lihachev.denissska@gmail.com>
2023-12-21 00:03:21 +0800

    Добавьте файлы проекта.

---
+++ TypeOfDate_Lab2/Program.cs
@@ -0,0 +1,38 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = new int[50];
+        Console.Write("Введите максимальное число: "); int max = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите минимальное число: "); int min = Convert.ToInt32(Console.ReadLine());
+        Console.Clear();
+        GetRandomArr(ref array, min, max);
+        ShowArr(array);
+        Console.ReadKey();
+    }
+    static void ShowArr(int[] arr)
+    {
+        for (int i = 0; i  < arr.Length; i++)
+        {
+            Console.WriteLine(arr[i] + " ");
+        }
+    }
+    static void GetRandomArr(ref int[] arr, int min, int max)
+    {
+        Random random = new Random();
+        for (int i = 0; i < arr.Length; i++)
+        {
+            var num = random.Next(min, max);
+
+            if (arr.Contains(num))
+            {
+                i--;
+            }
+            else
+            {
+                arr[i] = num;
+            }
+        }
+    }
+}
